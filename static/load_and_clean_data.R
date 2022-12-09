9b18e47658d42d45eede964e7a37773b560c4564
#Zhuoxi Zheng# <#zzheng99@bu.edu#>
2022-12-09 09:59:42 -0500

    Fri

--- static/load_and_clean_data.R
+++ static/load_and_clean_data.R
@@ -1,14 +1,25 @@
-install.packages("here")
-library(here)
 library(tidyverse)
-library(dplyr)
 
 ##DF1
-incomedata <- read_csv(here::here("dataset-ignore", "income.csv"))
+wage_data <- read_csv(here::here("dataset-ignore", "wage.csv"))
 
-## CLEAN the data
-loan_data_clean <- loan_data
+##DF2
+#CCR: completing college rate (after 25) 2016-2020
+#MHI: Median Household Income (2020)
+state_data <- read_csv(here::here("dataset", "states.csv"))
 
-write_csv(loan_data_clean, file = here::here("dataset", "loan_refusal_clean.csv"))
+## CLEAN DF1
+wage_data_clean1 <- 
+  wage_data %>% 
+  filter(INCWAGE < 999998)
 
-save(loan_data_clean, file = here::here("dataset/loan_refusal.RData"))
+summary(wage_data_clean1$INCWAGE)
+### outlier q0.75+1.5IQR=45000+1.5(45000-0)=112500
+wage_data_clean2 <- 
+  wage_data_clean1 %>% filter(INCWAGE < 112500)
+
+##Combine two data frames
+clean_wage <- merge(x = wage_data_clean2, y = state_data, by = "STATEFIP")
+
+write_csv(clean_wage, file = here::here("dataset-ignore", "clean_wage.csv"))
+save(clean_wage, file = here::here("dataset-ignore/clean_wage.RData"))
