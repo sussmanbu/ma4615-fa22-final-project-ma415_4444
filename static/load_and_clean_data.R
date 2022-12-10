225c663784114b59a2b48d1dc799676ff157d6b5
Xingru <xchen23@bu.edu>
2022-12-09 20:09:37 -0500

    modified df cols

--- static/load_and_clean_data.R
+++ static/load_and_clean_data.R
@@ -14,9 +14,34 @@ wage_data_clean1 <-
   filter(INCWAGE < 999998)
 
 summary(wage_data_clean1$INCWAGE)
+
 ### outlier q0.75+1.5IQR=45000+1.5(45000-0)=112500
+
 wage_data_clean2 <- 
   wage_data_clean1 %>% filter(INCWAGE < 112500)
+
+## change marriage status into 2 stages: married:1 unmarried:0
+wage_data_clean2$MARST <- with(wage_data_clean2, ifelse(MARST == 2, 1,
+                                                 ifelse(MARST == 1, 1, 0)))
+
+## add new col of SPEAKENG: 0: doesn't speak any; 1: speak well; 2: a little
+wage_data_clean2$SPEAKENG_new <- cut(wage_data_clean2$SPEAKENG,
+                                     breaks = c(0, 1, 2, 5, Inf),
+                                     labels = c(0, 1, 1, 1))
+
+## add new col of EDUC: 0: combined highschool into one
+library(dplyr)
+wage_data_clean2 <- wage_data_clean2 %>%
+  mutate(EDUC_new = case_when(EDUC == 1 ~ 1,
+                              EDUC == 2 ~ 2,
+                              EDUC == 3 ~ 3,
+                              EDUC %in% c(4, 5, 6) ~ 3,
+                              EDUC == 7 ~ 4,
+                              EDUC == 8 ~ 5,
+                              EDUC == 9 ~ 6,
+                              EDUC == 10 ~ 7,
+                                  TRUE ~ 8))
+
 
 ##Combine two data frames
 clean_wage <- merge(x = wage_data_clean2, y = state_data, by = "STATEFIP")
